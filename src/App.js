aa31ba348d044591bff3e27fd3e2b66779afbd30
wanjikuKim-collab <wanjikukimani24@gmail.com>
2023-11-30 10:55:33 +0300

    Initialized repo - created app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import React,{useState, useEffect} from 'react';
 import './App.css';
+
+//Auth Imports
+import Register from './Pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route path='/register' element={<Register />}></Route>      </Routes>
     </div>
   );
 }
