f90c7b11d2083f960d566b0f83b14aaaae3a3be3
wanjikuKim-collab <wanjikukimani24@gmail.com>
2023-12-01 00:39:27 +0300

    updated home page by adding transactions

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,50 +1,84 @@
 // src/components/Home.js
-// src/components/Home.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import { ClerkProvider, UserButton, SignedIn,
+   } from "@clerk/clerk-react";
+ 
+   import Transaction from './Transaction';
 
 const Home = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const storedTransactions = JSON.parse(localStorage.getItem('transactions')) || [];
+    setTransactions(storedTransactions);
+  }, []);
+  const [balance, setBalance] = useState()
+  // Fetch the balance of the account on page load
+  // useEffect(() => {
+  //   const storedBalance = JSON.parse(localStorage.getItem('balance')) || [];
+  //   setBalance(storedBalance);
+  // }, []);
+
   return (
-    <div className="home-container">
-      <h2 className="home-heading">Welcome to the Account Opening App</h2>
-
-      <div className="dashboard-sections">
-        {/* Account Overview */}
-        <div className="dashboard-section">
-          <h3 className="section-title">Account Overview</h3>
-          <p className="section-description">
-            View a summary of your account, including current balance, recent transactions, and account status.
-          </p>
+    <div className=" relative flex h-screen bg-gray-100">
+      {/* Sidebar */}
+      <div className='absolute top-3 right-3 '>
+          <UserButton/>
         </div>
+      <div className="w-1/4 bg-white border-r p-4">
+      
 
-        {/* Quick Actions */}
         <div className="dashboard-section">
-          <h3 className="section-title">Quick Actions</h3>
-          <ul className="section-list">
-            <li>Make a transaction</li>
-            <li>Update account information</li>
-            <li>View transaction history</li>
-          </ul>
-        </div>
-
-        {/* Notifications */}
-        <div className="dashboard-section">
-          <h3 className="section-title">Notifications</h3>
-          <p className="section-description">
-            Stay informed about account updates, transaction confirmations, and important announcements.
-          </p>
-        </div>
+            <h3 className="section-title">Quick Actions</h3>
+            <ul className="section-list">
+              <li>Make a transaction</li>
+              <Link to="/transactions" className="block p-2 bg-blue-500 text-white rounded-full">
+          Transactions
+           </Link>
+            </ul>
+          </div>
       </div>
 
-      {/* Action Buttons */}
-      <div className="action-buttons">
-        <Link to="/transactions" className="action-button">
-          <button>View Transactions</button>
-        </Link>
-        <Link to="/account-settings" className="action-button">
-          <button>Account Settings</button>
-        </Link>
+      {/* Main Dashboard */}
+      <div className="w-3/4 p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center mt-20">Welcome to the Account Opening App</h2>
+
+        {/* Dashboard Sections */}
+        <div className="flex-col">
+          {/* Account Overview */}
+          <div className="mb-10">
+            <h3 className="font-bold">Account Overview</h3>
+            <p className="section-description">
+              View a summary of your account, including current balance, recent transactions, and account status.
+            </p>
+            <div className='my-8'>
+            <p className=''> Balance</p>
+            <p className=''> ${balance}</p>
+            </div>
+            <div className=''>
+            {transactions.slice(-5).map((transaction, index) => (
+    <Transaction key={index} transaction={transaction} />
+  ))}
+  </div>
+          </div>
+
+          {/* Quick Actions */}
+          
+
+          {/* Notifications */}
+          <div className=" bg-slate-300 w-70 h-full">
+            <h3 className="font-bold">Notifications</h3>
+            <p className="font font-medium">
+              Stay informed about account updates, 
+              transaction confirmations, and important announcements.
+            </p>
+          </div>
+        </div>
+
+        {/* Action Buttons */}
+      
       </div>
     </div>
   );
