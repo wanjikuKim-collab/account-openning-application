0567e8753196388f932d622268d007490d3e1aef
wanjikuKim-collab <wanjikukimani24@gmail.com>
2023-11-30 13:47:54 +0300

    created account settings

---
+++ src/Pages/AccountSettings.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './AccountSettings.css';
+
+function AccountSettings() {
+  const [newPassword, setNewPassword] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+
+  const handleChangePassword = () => {
+    // Implement change password logic here
+  };
+
+  const handleChangeEmail = () => {
+    // Implement change email logic here
+  };
+
+  return (
+    <div>
+      <h2>Account Settings</h2>
+      <form>
+        <label>New Password:</label>
+        <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+
+        <button type="button" onClick={handleChangePassword}>
+          Change Password
+        </button>
+
+        <label>New Email:</label>
+        <input type="email" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
+
+        <button type="button" onClick={handleChangeEmail}>
+          Change Email
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AccountSettings;
